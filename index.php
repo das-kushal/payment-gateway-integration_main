c7ae9a060f12ccf925c86d7672001c86655e62d0
Kushal Das <86544278+das-kushal@users.noreply.github.com>
2022-01-01 17:57:51 +0530

    This is the main payment gateway integration
    This is done using razorpay integrated to php,html

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Cover Template · Bootstrap v5.1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      *{
+
+        color:black;
+      }
+      body{
+        background:url('donate.webp');
+        background-size:100% 100% ;
+        background-repeat:no-repeat;
+        background-attachment:fixed;
+        
+      }
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="mx-auto mb-0" style="font-size:4rem;">Spark Foundation Donation</h3>
+     
+      
+    </div>
+  </header>
+  <main class="px-3">
+    <h1>Donate for the poor.</h1>
+    <p class="lead">This is a donation page for the Spark Founadation internship with payment gateway integration to razor pay.</p>
+    <p class="lead">
+      <a href="https://rzp.io/l/2siXLdd" class="btn btn-lg btn-secondary fw-bold border-white bg-white"><span style="color:black">Donate Now</span></a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>&copy; 2022 <a href="#" class="text-dark">All Rights Reserved</a>, made with &hearts; by Kushal .Das</p>
+  </footer>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+  <!-- <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="https://rzp.io/l/J3h98WK" > </script>  -->
